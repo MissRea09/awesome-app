18ee86f541c0ad573b9646682701df4ba24b8210
Reagile Setalala <rea@Reagiles-MacBook-Air.local>
2025-09-04 10:56:37 +0200

    web fixes

--- src/compoents/navbar.tsx
+++ src/compoents/navbar.tsx
@@ -6,13 +6,13 @@ const Navbar: React.FC = () => {
     <nav className="flex justify-between items-center px-8 py-4 shadow-sm bg-white fixed top-0 left-0 right-0 z-50">
       {/* Logo Section */}
      <div className="flex items-center space-x-2 mr-auto">
-  <img src={knitLogo} alt="Knit Logo" className="h-10 w-auto" />
-  <span className="text-xl font-bold">Knit</span>
+  <img src={knitLogo} alt="Knit Logo" className="h-20 w-24" />
+  {/* <span className="text-xl font-bold">Knit</span> */}
 </div>
  
       {/* <div className="text-xl font-bold">Knit</div> */}
  
-      <ul className="hidden md:flex space-x-8 text-gray-700">
+      <ul className="hidden md:flex space-x-8 text-gray-700 mr-2">
         <li><a href="#home" className="hover:text-black">Home</a></li>
         <li><a href="#how-it-works" className="hover:text-black">How It Works for Parents</a></li>
         {/* <li><a href="#how-it-works-for-schools" className="hover:text-black">How It Works For Schools</a></li> */}
